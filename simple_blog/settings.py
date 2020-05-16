250eedd9cc57577349be80896f5d989de1506cd9
Daniel B <daniel.issab@gmail.com>
2020-05-16 20:29:17 -0300

    static root

--- simple_blog/settings.py
+++ simple_blog/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -23,9 +24,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '4u*=$7c0s=$#o+xzpsk@i-g$-#066xy9vrs+!_z6ab-s&#f52!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
 
 
 # Application definition
@@ -119,7 +120,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATIC_URL = '/static/'
 
 # to make summernote don't trigger clickjacking protection
 X_FRAME_OPTIONS = 'SAMEORIGIN'
+#django_heroku.settings(locals())
